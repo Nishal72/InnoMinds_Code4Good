4fd479c35911822a055caa8d0a26c20df6cf70fe
Abdurrahman-gurib <noorgurib@gmail.com>
2025-11-15 18:31:55 +0400

    GreenLoan Commits

--- green_loan/admin.py
+++ green_loan/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import GreenLoan
 
-# Register your models here.
+@admin.register(GreenLoan)
+class GreenLoanAdmin(admin.ModelAdmin):
+    list_display = ('user', 'employee_name', 'monthly_salary', 'loan_type', 'loan_available', 'max_loan_amount', 'created_at')
+    list_filter = ('loan_available', 'loan_type', 'created_at')
+    search_fields = ('user__username', 'employee_name', 'employee_id', 'company_name')
+    readonly_fields = ('created_at', 'updated_at', 'monthly_payment')
+    
+    fieldsets = (
+        ('User Information', {
+            'fields': ('user',)
+        }),
+        ('Payslip Data', {
+            'fields': ('payslip_image', 'payslip_text')
+        }),
+        ('Employee Information', {
+            'fields': ('employee_name', 'employee_id', 'monthly_salary', 'company_name', 'designation')
+        }),
+        ('Loan Analysis', {
+            'fields': ('loan_suggestion', 'loan_available', 'loan_type', 'interest_rate', 'max_loan_amount', 'loan_term_months', 'monthly_payment')
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
+    
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
--- green_loan/urls.py
+++ green_loan/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('green_loan/', views.green_loan_view, name='green_loan'),
+    path('api/extract-payslip/', views.extract_payslip_text, name='extract_payslip'),
+    path('api/analyze-payslip/', views.analyze_payslip, name='analyze_payslip'),
 ]
