938cc512fe28b20c4cc8ff7504d234a0ccc24cd7
Nishal <nishal72babajee@gmail.com>
2025-11-16 00:20:56 +0400

    Clean the landing page and made it relevant from the old template

--- core/views.py
+++ core/views.py
@@ -6,8 +6,8 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-def courses(request):
-    return render(request, 'courses.html')
+# def courses(request):
+#     return render(request, 'courses.html')
 
 def contact(request):
     return render(request, 'contact.html')
